c43f6ec2d4501f7f255dc4082d8e44de3f6fdf1c
torrixo <santy.torrico@gmail.com>
2023-03-04 01:49:26 -0400

    Created search box

--- app/src/main/java/com/example/guias/BackgroundActivity.kt
+++ app/src/main/java/com/example/guias/BackgroundActivity.kt
@@ -3,6 +3,9 @@ package com.example.guias
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -33,4 +36,19 @@ class BackgroundActivity : AppCompatActivity() {
             layoutPrincipal.setBackgroundColor(Color.WHITE)
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.menu_item1 -> {
+                Log.d("TEST", "PRESS ICON MENU")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
